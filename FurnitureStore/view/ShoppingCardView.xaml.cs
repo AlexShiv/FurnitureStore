10174889d0e94ff01aadf8e19b1cada115997a60
Alexey Shiyanov <shiyanov1998@mail.ru>
2022-02-08 19:25:04 +0400

    Выравнивание элементов корзины

--- FurnitureStore/view/ShoppingCardView.xaml.cs
+++ FurnitureStore/view/ShoppingCardView.xaml.cs
@@ -24,81 +24,78 @@ namespace FurnitureStore.view
                         var nameLabel = new Label
                         {
                             FontSize = 18,
-                            WidthRequest = mainDisplayInfo.Width / 4,
-                            HorizontalOptions = LayoutOptions.Center,
-                            VerticalOptions = LayoutOptions.CenterAndExpand,
                             VerticalTextAlignment = TextAlignment.Center,
-                            HorizontalTextAlignment = TextAlignment.Center
+                            HorizontalTextAlignment = TextAlignment.Center,
+                            HorizontalOptions = LayoutOptions.FillAndExpand,
                         };
                         nameLabel.SetBinding(Label.TextProperty, "Furniture.Name");
 
                         var priceLabel = new Label
                         {
                             FontSize = 18,
-                            WidthRequest = mainDisplayInfo.Width / 4,
-                            HorizontalOptions = LayoutOptions.Center,
-                            VerticalOptions = LayoutOptions.CenterAndExpand,
                             VerticalTextAlignment = TextAlignment.Center,
-                            HorizontalTextAlignment = TextAlignment.Center
+                            HorizontalTextAlignment = TextAlignment.Center,
+                            HorizontalOptions = LayoutOptions.FillAndExpand
                         };
                         priceLabel.SetBinding(Label.TextProperty, "Furniture.Price");
                         
                         var countLabel = new Label
                         {
-                            FontSize = 18,
-                            WidthRequest = mainDisplayInfo.Width / 4,
+                            FontSize = 15,
+                            // BackgroundColor = Color.Chartreuse,
                             VerticalTextAlignment = TextAlignment.Center,
                             HorizontalTextAlignment = TextAlignment.Center,
-                            BackgroundColor = Color.Chartreuse,
                         };
                         countLabel.SetBinding(Label.TextProperty, "Count");
 
                         var image = new Image
                         {
                             HeightRequest = 100,
-                            WidthRequest = mainDisplayInfo.Width / 4,
-                            HorizontalOptions = LayoutOptions.Center,
-                            VerticalOptions = LayoutOptions.CenterAndExpand,
-                            BackgroundColor = Color.Brown,
-                            Margin = new Thickness(0,30)
+                            WidthRequest = 100,
+                            // BackgroundColor = Color.Brown,
                         };
                         image.SetBinding(Image.SourceProperty, "Furniture.PhotoPath");
 
 
                         var buttons = new StackLayout()
                         {
-                            BackgroundColor = Color.Blue,
+                            // BackgroundColor = Color.Blue,
                             Orientation = StackOrientation.Vertical,
                             Children =
                             {
                                 new Button()
                                 {
+                                    HeightRequest = 40,
+                                    WidthRequest = 40,
                                     Text = "+",
                                     FontSize = Device.GetNamedSize(NamedSize.Small, typeof(Button)),
                                     BorderWidth = 1,
-                                    Margin = new Thickness(10),
                                 },
                                 countLabel,
                                 new Button()
                                 {
+                                    HeightRequest = 40,
+                                    WidthRequest = 40,
                                     Text = "-",
                                     FontSize = Device.GetNamedSize(NamedSize.Small, typeof(Button)),
                                     BorderWidth = 1,
-                                    Margin = new Thickness(10),
                                 }
                             },
-                            WidthRequest = mainDisplayInfo.Width / 4.5,
+                            Padding = new Thickness(10),
                         };
-                        var row = new FlexLayout()
+                        var row = new StackLayout()
                         {
+                            // BackgroundColor = Color.Chocolate,
+                            Orientation = StackOrientation.Horizontal,
                             Padding = new Thickness(10, 10),
                             Children = { image, nameLabel, priceLabel, buttons },
                             WidthRequest = mainDisplayInfo.Width,
-                            HorizontalOptions = LayoutOptions.Center,
-                            VerticalOptions = LayoutOptions.CenterAndExpand,
+                            HorizontalOptions = LayoutOptions.FillAndExpand,
                             
                         };
 
+                        // TODO название наверху
+                        // TODO добавить кнопку удаления товара из корзины
                         return new ViewCell
                         {
                             View = row
