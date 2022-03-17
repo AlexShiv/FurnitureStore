171b82ac1a84d89c2e7d76f749bf480d86232c92
Alexey Shiyanov <shiyanov1998@mail.ru>
2022-03-17 21:11:08 +0400

    Добавление и удаление категорий

--- FurnitureStore/view/CategoryView.xaml.cs
+++ FurnitureStore/view/CategoryView.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
 using FurnitureStore.config;
 using FurnitureStore.model;
 using FurnitureStore.repository;
+using FurnitureStore.viewModel;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -13,9 +16,8 @@ namespace FurnitureStore.view
         public CategoryView()
         {
             InitializeComponent();
-            var categoryRepository = CategoryRepository.GetInstance();
 
-            var categories = categoryRepository.Fetch();
+            var categories = VmCategory.Category;
             var listView = new ListView
             {
                 HasUnevenRows = true,
@@ -41,20 +43,107 @@ namespace FurnitureStore.view
                 })
             };
             listView.ItemSelected += ListView_ItemTapped;
-            
-            Content = new StackLayout
+
+
+            var footer = BuildFooter();
+            if (VmUser.IsAdmin())
             {
+                Content = new StackLayout
+                {
+                    Children =
+                    {
+                        listView, footer
+                    }
+                };
+            }
+            else
+            {
+                Content = new StackLayout
+                {
+                    Children =
+                    {
+                        listView
+                    }
+                };
+            }
+        }
+
+        private StackLayout BuildFooter()
+        {
+            var addCategory = new Button
+            {
+                Text = "Добавить категорию",
+                FontSize = Device.GetNamedSize(NamedSize.Micro, typeof(Button)),
+                BorderWidth = 1,
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center,
+                
+            };
+            addCategory.Clicked += OnAddButtonClicked;
+            
+            var removeCategory = new Button
+            {
+                Text = "Удалить категорию",
+                FontSize = Device.GetNamedSize(NamedSize.Micro, typeof(Button)),
+                BorderWidth = 1,
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center,
+            };
+            removeCategory.Clicked += OnRemoveButtonClicked;
+            
+            return new StackLayout()
+            {
+                Orientation = StackOrientation.Horizontal,
+                HorizontalOptions = LayoutOptions.Center,
+                Margin = 10,
                 Children =
                 {
-                    listView
+                    addCategory, removeCategory
                 }
             };
         }
-        
+
+        private async void OnAddButtonClicked(object sender, EventArgs e)
+        {
+            try
+            {
+                string result = await DisplayPromptAsync("Добавление новой категории", "Введите название новой категории");
+                var categoryRepository = CategoryRepository.GetInstance();
+                categoryRepository.SaveItem(new Category()
+                {
+                    Name = result
+                });
+                VmCategory.UpdateList(categoryRepository.Fetch().ToList());
+                await DisplayAlert("Добавление выполнено успешно", "Нажмите \"OK\" для продолжения", "OK");
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+                await DisplayAlert("Ошибка при добавлении новой категории", "Нажмите \"OK\" для продолжения", "OK");
+            }
+        }
+        private async void OnRemoveButtonClicked(object sender, EventArgs e)
+        {
+            string result = await DisplayPromptAsync("Удаление категории", "Введите название удаляемой категории");
+            var categoryRepository = CategoryRepository.GetInstance();
+            var categories = categoryRepository.Fetch().ToList();
+            if (categories.Exists(category => category.Name == result))
+            {
+                categoryRepository.Delete(categories.Find(category => category.Name == result).Id);
+                VmCategory.UpdateList(categoryRepository.Fetch().ToList());
+                await DisplayAlert("Удаление выполнено успешно", "Нажмите \"OK\" для продолжения", "OK");
+            }
+            else
+            {
+                await DisplayAlert("Категория с таким именем не обнаружена!", "Нажмите \"OK\" для продолжения", "OK");
+
+            }
+        }
+
         private async void ListView_ItemTapped(object sender, SelectedItemChangedEventArgs e)
         {
             if (e.SelectedItem is Category category)
-                 await Navigation.PushAsync(new FurnitureView(category.Id));
+                await Navigation.PushAsync(new FurnitureView(category.Id));
         }
     }
 }
